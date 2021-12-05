69bc94c291f42b25ce0999d05cef182f500b337d
Igor Hristov <58328456+igorhristov@users.noreply.github.com>
2021-12-05 17:22:48 +0100

    Attributes List initial state and style in product details

---
+++ src/redux/attributes/attributes.types.js
@@ -0,0 +1,5 @@
+const AttributesActionTypes = {
+  LIST_SELECTED_ATTRIBUTES: "LIST_SELECTED_ATTRIBUTES",
+};
+
+export default AttributesActionTypes;
--- src/redux/root-reducers.js
+++ src/redux/root-reducers.js
@@ -1,12 +1,20 @@
 import { combineReducers } from "redux";
 
 import cartReducer from "./cart/cart.reducer";
+
 import currencyReducer from "./currency/currency.reducer";
-import { productListReducer, productDetailsReducer } from "./product/product.reducer";
+
+import {
+  productListReducer,
+  productDetailsReducer,
+} from "./product/product.reducer";
+
+import { attributesListReducer } from "./attributes/attributes.reducer";
 
 export default combineReducers({
   cart: cartReducer,
   currencies: currencyReducer,
   productList: productListReducer,
-  productDetails: productDetailsReducer
+  productDetails: productDetailsReducer,
+  attributesList: attributesListReducer,
 });
